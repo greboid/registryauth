1c5e68f7bdce6f0755e99bdfdb5e3aa483d521cf
Greg Holmes <git@greg.holmes.name>
2021-08-01 19:50:20 +0100

    Show debug data when debug is enabled

--- auth/logging.go
+++ auth/logging.go
@@ -12,7 +12,7 @@ type Formatter struct {
 
 func (f Formatter) Format(entry *log.Entry) ([]byte, error) {
 	if f.Debug {
-		return []byte(fmt.Sprintf("%s %s: %s\n", entry.Time.Format("2006/01/02 15:04:05"), entry.Level, entry.Message)), nil
+		return []byte(fmt.Sprintf("%s %s: %s [%s]\n", entry.Time.Format("2006/01/02 15:04:05"), entry.Level, entry.Message, entry.Data)), nil
 	}
-	return []byte(fmt.Sprintf("%s %s: %s [%s]\n", entry.Time.Format("2006/01/02 15:04:05"), entry.Level, entry.Message, entry.Data)), nil
+	return []byte(fmt.Sprintf("%s %s: %s\n", entry.Time.Format("2006/01/02 15:04:05"), entry.Level, entry.Message)), nil
 }
