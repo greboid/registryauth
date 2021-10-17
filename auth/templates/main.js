a17e09df66bb57db77c3a2a4173edfb3e273d0ac
Greg Holmes <git@greg.holmes.name>
2021-10-17 03:36:29 +0100

    Add copy support (copies registry/image:sha)

--- auth/templates/main.js
+++ auth/templates/main.js
@@ -2,4 +2,18 @@ document.addEventListener("click", (event) => {
     if (event.target.classList.contains("tagList")) {
         event.target.closest("td").classList.toggle("showinfo")
     }
+    if (event.target.classList.contains("copy")) {
+        if (!navigator.clipboard) {
+            console.log("Unable to copy")
+        }
+        let sha = event.target.previousElementSibling.textContent
+        let tag = event.target.previousElementSibling.previousElementSibling.textContent
+        let registry = document.title
+        navigator.clipboard.writeText(`${registry}/${tag}:${sha}`)
+            .catch(e => console.log(`Error copying ${e}`))
+    }
 });
+
+const copyText = (text) => {
+
+};
