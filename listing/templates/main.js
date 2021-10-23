015822c769c9b1f35110a2cccde4f15b70638bcb
Greg Holmes <git@greg.holmes.name>
2021-10-23 01:17:14 +0100

    Fix copy to clipboard code a little

--- listing/templates/main.js
+++ listing/templates/main.js
@@ -3,12 +3,12 @@ document.addEventListener("click", (event) => {
         if (!navigator.clipboard) {
             console.log("Unable to copy")
         }
-        let sha = event.target.previousElementSibling.textContent
-        let tag = event.target.previousElementSibling.previousElementSibling.textContent
-        let registry = document.title
-        navigator.clipboard.writeText(`${registry}/${tag}:${sha}`)
+        let sha = event.target.previousElementSibling.textContent.trim()
+        let image = event.target.closest('tr').children[0].textContent.trim()
+        let registry = document.title.trim()
+        navigator.clipboard.writeText(`${registry}/${image}@${sha}`)
             .catch(e => console.log(`Error copying ${e}`))
-    } else if (event.target.closest("td").classList.contains("tags")) {
+    } else if (event.target.closest("td") != null && event.target.closest("td").classList.contains("tags")) {
         event.target.closest("td").classList.toggle("showinfo")
     }
 });
